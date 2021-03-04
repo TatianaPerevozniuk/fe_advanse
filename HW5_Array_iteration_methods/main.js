7424b3d6807eac364b457036b69fc594882dbafc
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-03-04 14:44:52 +0200

    removed unnecessary comments

--- HW5_Array_iteration_methods/main.js
+++ HW5_Array_iteration_methods/main.js
@@ -1,6 +1,5 @@
-//1. Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел. У функції є параметри:
-//     length - довжина масиву, min – мінімальне значення цілого числа, max – максимальне значення цілого числа.
-//     Приклад: getRandomArray(15, 1, 100) –> [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2]
+document.write(`<h1>HW5_Array_iteration_methods </br></h1>`);
+//1
 function getRandomArray(length = 0, min = 0, max = 0) {
     if (Number.isInteger(length) && length >= 0 && Number.isInteger(min) && Number.isInteger(max)){
         let randomArray = [];
@@ -12,64 +11,58 @@ function getRandomArray(length = 0, min = 0, max = 0) {
 }
 const randomArray = getRandomArray(8, 10,-30);
 console.log(randomArray);
+document.write(`Function №1:  getRandomArray(8, 10,-30) => ${randomArray} </br>`);
 
 //2.
+document.write(`Function №2: {} </br>`);
 
-
-//3. Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів.
-//     НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
-// Приклад: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4
+//3
 function getAverage(...numbers){
     const newArray = numbers.filter(number => Number.isInteger(number));
     return Number(newArray.reduce((accumulator,number) => accumulator + number / newArray.length,0).toFixed(2));
 }
-console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+const average = getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2);
+console.log(average);
+document.write(`Function №3:  getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) => ${average} </br>`);
 
-
-// 4. Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
-// Приклад: getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 23 Приклад: getMedian(1, 2, 3, 4) –> 2.5
-// Приклад: getMedian(1, 2, 3, 4, 5) –> 3
+// 4
 function getMedian(...numbers) {
     const newArray = numbers.filter(number => Number.isInteger(number));
     newArray.sort((a,b) => a-b);
-
     const lowMiddle = Math.floor( (newArray.length - 1) / 2);
     const highMiddle = Math.ceil( (newArray.length - 1) / 2);
     return ( newArray[lowMiddle] + newArray[highMiddle]) / 2;
 }
-console.log(getMedian(6, 2, 55, 11, 'jhj', 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-console.log(getMedian(1, 2, 3, 4, 6.7, 5));
-console.log(getMedian(1, 2, 3, 4));
+const median = getMedian(6, 2, 55, 11, 'jhj', 78, 2, 6.7, 55, 77, 57, 87, 23, 2, 56, 3, 2);
+console.log(median);
+document.write(`Function №4: getMedian(6, 2, 55, 11, 'jhj', 78, 2, 6.7, 55, 77, 57, 87, 23, 2, 56, 3, 2) => ${median} </br>`);
 
-// 5. Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
-// Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]
+//5
 function filterEvenNumbers(...numbers) {
     return numbers.filter(number => number%2 && Number.isInteger(number));
 }
-console.log(filterEvenNumbers(1, 2, -3, 4, 5, 4.3,'apple','7', 6));
+const evenNumbers = filterEvenNumbers(1, 2, -3, 4, 5, 4.3,'apple','7', 6);
+console.log(evenNumbers);
+document.write(`Function №5: filterEvenNumbers(1, 2, -3, 4, 5, 4.3,'apple','7', 6) => ${evenNumbers} </br>`);
 
-//6.Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
-// Приклад: countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3
+//6
 function countPositiveNumbers(...numbers) {
     return numbers.filter(number => number > 0 && typeof number === 'number').length;
 }
-console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6, 6.7, '5', 'apple'));
+const countPositive = countPositiveNumbers(1, -2, 3, -4, -5, 6, 6.7, '5', 'apple');
+console.log(countPositive);
+document.write(`Function №6: countPositiveNumbers(1, -2, 3, -4, -5, 6, 6.7, '5', 'apple') => ${countPositive} </br>`);
 
-//7.Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті,
-// які діляться на ціло на 5
-// Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
+//7
 function getDividedByFive(...numbers) {
     return numbers.filter(number => number%5 === 0 && Number.isInteger(number));
 }
-console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 5.5, '5','apple'));
+const dividedByFive = getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 5.5, '5','apple');
+console.log(dividedByFive);
+document.write(`Function №7: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 5.5, '5','apple')) => 
+${dividedByFive} </br>`);
 
-//8.Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*).
-// При вирішенні цього завдання необхідно розбити масив на слова за допомогою функції .split(" "), після чого масив
-// необхідно буде склеїти .join(" ") Погані слова: shit та fuck. Передбачте можливість розширювати список цих слів
-// у майбутньому.
-// Приклад: replaceBadWords("Are you fucking kidding?") -> "Are you ****ing kidding?"
-// Приклад: replaceBadWords("Holy shit!") -> "Holy ****!"
-// Приклад: replaceBadWords("It's bullshit!") -> "It's bull****!"
+//8
 function replaceBadWords(string) {
     let newArr = string.split(' ');
     newArr =  newArr.map((word) => {
@@ -78,4 +71,12 @@ function replaceBadWords(string) {
     }).join(' ');
     return newArr[0].toUpperCase() + newArr.slice(1);
 }
-console.log(replaceBadWords("Holy shit Fucking! It's bullshit!"));
+const replaceBadWord = replaceBadWords("Holy shit Fucking! It's bullshit!");
+console.log(replaceBadWord);
+document.write(`Function №8: replaceBadWords("Holy shit Fucking! It's bullshit!") => ${replaceBadWord} </br>`);
+
+//9.
+document.write(`Function №9: {} </br>`);
+
+//10.
+document.write(`Function №10: {} </br>`);
