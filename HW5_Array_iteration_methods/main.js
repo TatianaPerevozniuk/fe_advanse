f8db6d74cd84059aae7077c6a36d309092071b99
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-03-04 11:39:19 +0200

    created function filterEvenNumbers

--- HW5_Array_iteration_methods/main.js
+++ HW5_Array_iteration_methods/main.js
@@ -43,3 +43,7 @@ console.log(getMedian(1, 2, 3, 4));
 
 // 5. Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
 // Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]
+function filterEvenNumbers(...numbers) {
+    return numbers.filter(number => {if (number%2 && Number.isInteger(number)) return number;});
+}
+console.log(filterEvenNumbers(1, 2, -3, 4, 5, 4.3,'fdvf','7', 6));
