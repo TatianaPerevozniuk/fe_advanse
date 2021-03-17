d51a0b1ae06999b8d914490f79b9c2fa73d13bcc
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-03-17 20:46:03 +0200

    A new class Budget Student has been created that inherits from the Student class. Added the getScholarship method which displays information about the scholarship.

--- HW8_Prototypes_ES6Classes/main.js
+++ HW8_Prototypes_ES6Classes/main.js
@@ -1,34 +1,37 @@
-
 class Student {
-    constructor(university, course, fullName) {
+    constructor(university, course, fullName, marks = []) {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
-        this.marks = [];
+        this.marks = marks;
         this.dismissed = false;
         this.dismiss = function () {
             this.dismissed = true;
         };
-        this.recover = function (){
+        this.recover = function () {
             this.dismissed = false;
             this.marks = [];
         };
     }
+
     get getInfo() {
         return `${this.course}th year student of the ${this.university}, ${this.fullName}.`;
     }
+
     get getMarks() {
-        return this.dismissed? this.marks = null : this.marks;
+        return this.dismissed ? this.marks = null : this.marks;
     }
+
     set setMarks(mark) {
-        !this.dismissed ? this.marks.push(mark): this.marks = null;
+        !this.dismissed ? this.marks.push(mark) : this.marks = null;
     }
+
     static getAverageMark(marks) {
-        return marks !== null?Number((marks.reduce((sum, mark) => (sum + mark)) / marks.length).toFixed(2)):null;
+        return marks !== null ? Number((marks.reduce((sum, mark) => (sum + mark)) / marks.length).toFixed(2)) : null;
     }
 }
 
-const tania = new Student('Kiev Polytechnic Institute', 5,'Perevozniuk Tatiana Igorevna');
+const tania = new Student('Kiev Polytechnic Institute', 5, 'Perevozniuk Tatiana Igorevna');
 console.log(tania);
 const infoTania = tania.getInfo;
 console.log(infoTania);
@@ -39,25 +42,57 @@ tania.setMarks = 5;
 tania.setMarks = 5;
 const marksTania = tania.getMarks;
 const averageMarkTania = Student.getAverageMark(tania.marks);
-console.log('Marks: ',marksTania,'; Average Mark: ',averageMarkTania);
+console.log('Marks: ', marksTania, '; Average Mark: ', averageMarkTania);
 
 tania.dismiss();
 tania.setMarks = 5;
-console.log('Student dismissed. Marks: ',tania.getMarks,'; Average Mark: ',Student.getAverageMark(tania.marks));
+console.log('Student dismissed. Marks: ', tania.getMarks, '; Average Mark: ', Student.getAverageMark(tania.marks));
 
 tania.recover();
 tania.setMarks = 5;
 tania.setMarks = 4;
 tania.setMarks = 5;
-console.log('Student recovered. Marks: ',tania.getMarks,'; Average Mark: ',Student.getAverageMark(tania.marks));
+console.log('Student recovered. Marks: ', tania.getMarks, '; Average Mark: ', Student.getAverageMark(tania.marks));
 
+console.log(tania);
 
 // Advanced
-//
-// 1. Створіть новий клас BudgetStudent, який повністю наслідує клас Student
-// 2. Бюджетний студент може отримати стипендію за допомогою методу this.getScholarship.
-//     Отримання стипендії супроводжується виведенням інформації в консоль: Ви отримали 1400 грн. стипендії
-// 3. Метод отримання стипендії автоматично викликається кожні 30 секунд післе створення об'єкту.
-//    Підказка: викликайте його в constructor
-// 4. Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0
-// 5. Якщо студента виключено, він не отримує стипендію (думаю це було і так очевидно :) )
+class BudgetStudent extends Student {
+    constructor(university, course, fullName) {
+        super(university, course, fullName);
+        setInterval(() => this.getScholarship,30000)
+    }
+
+    get getScholarship() {
+        if (this.dismissed) console.log(`${this.fullName} -> Student dismissed.`);
+        else if (this.marks.length === 0) console.log(`${this.fullName} -> No marks.You do not receive a scholarship.`);
+        else if (Student.getAverageMark(this.marks) >= 4 && !this.dismissed) {
+            console.log(`${this.fullName} -> You received 1400uah scholarships.`);
+        }
+        else {
+            console.log(`${this.fullName} -> You do not receive a scholarship.Your average mark is ${Student.getAverageMark(this.marks)} < 4.`);
+        }
+    }
+}
+
+const anna = new BudgetStudent('KPI', 5, 'Anna');
+anna.dismiss();
+console.log(anna);
+
+const vlad = new BudgetStudent('KPI', 2, 'Vladislav');
+console.log(vlad);
+
+const mary = new BudgetStudent('KPI', 3, 'Mary');
+mary.setMarks = 5;
+mary.setMarks = 5;
+mary.setMarks = 3;
+console.log(mary);
+
+const den = new BudgetStudent('KPI', 4, 'Den');
+den.setMarks = 5;
+den.setMarks = 3;
+den.setMarks = 3;
+console.log(den);
+
+
+
