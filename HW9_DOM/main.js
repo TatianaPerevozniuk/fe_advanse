4bf811d0c75e5185185ee4bdf0144810a46bbfc1
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-03-19 00:08:19 +0200

    The generateBlocksInterval() function was created. It is called when the 'Generate Blocks Interval' button is pressed and randomly changes the blocks color at intervals.

--- HW9_DOM/main.js
+++ HW9_DOM/main.js
@@ -1,15 +1,20 @@
-// Зродіть 25 квадратів розміру 50х50 пікселів кожен, зафарбовані у випадковий колір.
-// Квадрати мають розташовуватись по 5 в ряд.
-// Щоб квадрати з'явились на сторінці, необхідно викликати функцію generateBlocks();
+
 function generateBlocks() {
     const grid = document.querySelector('.grid');
     grid.style.display = 'grid';
+    blockRandomColor();
 }
 
 // Advanced
-// зробіть так, щоб квадрати змінювали колір раз на секунду.
-// Щоб квадрати з'явились на сторінці та почали змінюватись, необхідно викликати функцію generateBlocksInterval();
-
-function generateBlocksInterval() {
-
+function blockRandomColor() {
+    const grid = document.querySelectorAll('.block');
+    grid.forEach(item => item.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, 
+                                                                    ${getRandomColor(0, 255)}, 
+                                                                    ${getRandomColor(0, 255)})`)
+}
+function getRandomColor(min, max){
+    return Math.ceil(Math.random() * (max - min) + min);
+}
+function generateBlocksInterval(time = 1000) {
+    setInterval(() => blockRandomColor(), time);
 }
