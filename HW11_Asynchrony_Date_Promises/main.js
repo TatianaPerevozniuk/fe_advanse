975a699b60401ebbe4fbc53e22389b1d67111557
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-03-26 22:29:55 +0200

    added HW11_Asynchrony_Date_Promises/main.js

--- HW11_Asynchrony_Date_Promises/main.js
+++ HW11_Asynchrony_Date_Promises/main.js
@@ -6,3 +6,23 @@
 // Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами.
 // Час роботи проміса має складати length * 50ms.
 // (Якщо викликати getRandomChinese(4), отримаємо результат "촛궻簽紙" за 200ms
+
+
+async function getRandomChinese(length) {
+    let arrayRandomChineseNumber = [];
+    for (let i = 0; i < length; i++) {
+        const randomNumber = Number.parseInt(Date.now().toString().slice(-5));
+        const randomChineseNumber = String.fromCharCode(randomNumber);
+        arrayRandomChineseNumber.push(randomChineseNumber);
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(randomChineseNumber)
+            }, 50)
+        })
+    }
+    console.log(arrayRandomChineseNumber.join(''));
+}
+getRandomChinese(4);
+
+
+
